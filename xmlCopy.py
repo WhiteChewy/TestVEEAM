55214a724c82c1048c2e461aa845a3a365507407
Nikita <pocketkurt@gmail.com>
2021-06-29 20:36:43 +0300

    added multiprocessing version

--- xmlCopy.py
+++ xmlCopy.py
@@ -2,11 +2,17 @@
 import xml.etree.ElementTree as ElementTree
 import shutil
 
-tree = ElementTree.parse("file.xml")
-root = tree.getroot()
 
-for node in root:
-    if node.attrib['source_path'][0] == '/':
-        shutil.copy2(node.attrib['source_path'] + '/' + node.attrib['file_name'], node.attrib['destination_path'])
-    else:
-        shutil.copy2(node.attrib['source_path']+'\\'+node.attrib['file_name'], node.attrib['destination_path'])
+def xmlCopy(XmlTreeRoot):
+    for node in XmlTreeRoot:
+        if node.attrib['source_path'][0] == '/':
+             shutil.copy2(node.attrib['source_path'] + '/' + node.attrib['file_name'], node.attrib['destination_path'])
+        else:
+            shutil.copy2(node.attrib['source_path']+'\\'+node.attrib['file_name'], node.attrib['destination_path'])
+
+
+if __name__ == "__main__":
+    tree = ElementTree.parse("file.xml")
+    root = tree.getroot()
+    xmlCopy(root)
+
---
+++ xmlCopyMP.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from multiprocessing import Process
+import xml.etree.ElementTree as ElementTree
+import shutil
+
+
+def xmlCopy(XmlTreeRoot):
+    for node in XmlTreeRoot:
+        if node.attrib['source_path'][0] == '/':
+            shutil.copy2(node.attrib['source_path'] + '/' + node.attrib['file_name'], node.attrib['destination_path'])
+        else:
+            shutil.copy2(node.attrib['source_path'] + '\\' + node.attrib['file_name'], node.attrib['destination_path'])
+
+
+if __name__ == "__main__":
+    tree = ElementTree.parse("file.xml")
+    root = tree.getroot()
+    p = Process(target=xmlCopy, args=(root,))
+    p.start()
+    p.join()
+
