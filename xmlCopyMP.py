3797774fb8c985e3d01431405de8eef06286e993
Nikita <pocketkurt@gmail.com>
2021-06-29 22:39:50 +0300

    multithreading fix

--- xmlCopyMP.py
+++ xmlCopyMP.py
@@ -4,18 +4,29 @@ import xml.etree.ElementTree as ElementTree
 import shutil
 
 
-def xmlCopy(xmlTreeRoot):
+def xmlTreeToList(xmlTreeRoot):
+    nodeList = []
     for node in xmlTreeRoot:
-        if node.attrib['source_path'][0] == '/':
-            shutil.copy2(node.attrib['source_path'] + '/' + node.attrib['file_name'], node.attrib['destination_path'])
+        nodeList.append(node.attrib)
+    return nodeList
+
+
+def fileCopy(nodes):
+    for node in nodes:
+        if node['source_path'][0] == '/':
+            shutil.copy2(node['source_path'] + '/' + node['file_name'], node['destination_path'])
         else:
-            shutil.copy2(node.attrib['source_path'] + '\\' + node.attrib['file_name'], node.attrib['destination_path'])
+            shutil.copy2(node['source_path'] + '\\' + node['file_name'], node['destination_path'])
 
 
 if __name__ == "__main__":
     tree = ElementTree.parse("file.xml")
     root = tree.getroot()
-    p = Process(target=xmlCopy, args=(root,))
-    p.start()
-    p.join()
+    nodes = xmlTreeToList(root)
+    p1 = Process(target=fileCopy, args=([nodes[i] for i in range(0, len(nodes), 2)],))
+    p2 = Process(target=fileCopy, args=([nodes[i] for i in range(1, len(nodes), 2)],))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
 
